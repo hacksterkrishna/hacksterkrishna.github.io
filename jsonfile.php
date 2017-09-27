aa2d45b6dd76b4f565354a95b79d3ecc64cd6428
krishna hackster <krishnagek@gmail.com>
2017-09-28 02:01:28 +0530

    Create jsonfile.php

---
+++ jsonfile.php
@@ -0,0 +1,50 @@
+{
+"error": false,
+"heroes":[
+{
+"name": "Spiderman",
+"about": "Spiderman is a fictional superhero appearing in american comic books.",
+"image": "http://placehold.it/600/92c952"
+},
+{
+"name": "superman",
+"about": "supermanis a fictional superhero appearing in american comic books.",
+"image": "http://placehold.it/600/771796"
+},
+{
+"name": "monkeyman",
+"about": "monkey man is a fictional superhero appearing in american comic books.",
+"image": "http://placehold.it/600/24f355"
+},
+{
+"name": "donkey man",
+"about": "donkeyman is a fictional superhero appearing in american comic books.",
+"image": "http://placehold.it/600/d32776"
+},
+{
+"name": "cowman",
+"about": "cowman is a fictional superhero appearing in american comic books.",
+"image": "http://placehold.it/600/f66b97"
+},
+{
+"name": "specialman",
+"about": "Specialman is a fictional superhero appearing in american comic books.",
+"image": "http://placehold.it/600/56a8c2"
+},
+{
+"name": "Steelman",
+"about": "Steelman is a fictional superhero appearing in american comic books.",
+"image": "http://placehold.it/600/b0f7cc"
+},
+{
+"name": "Sron man",
+"about": "iron man is a fictional superhero appearing in american comic books.",
+"image": "http://placehold.it/600/54176f"
+},
+{
+"name": "flyman",
+"about": "flyman is a fictional superhero appearing in american comic books.",
+"image": "http://placehold.it/600/51aa97"
+}
+]
+}
